24d6f20c258a9e25abd18eb8cffb55f01e4b8f8b
teresa <teresa.juengel@posteo.de>
2023-12-11 13:03:54 +0100

    add status to todo

--- src/Todo.ts
+++ src/Todo.ts
@@ -2,5 +2,11 @@ export type Todo =
     {
         id?: string;
         description?: string;
-        status?: string;
+        status?: TodoStatus;
     }
+
+export enum TodoStatus {
+    OPEN = "OPEN",
+    IN_PROGRESS = "IN_PROGRESS",
+    DONE = "DONE",
+}
--- src/TodoForm.tsx
+++ src/TodoForm.tsx
@@ -1,21 +1,26 @@
 import {ChangeEvent, FormEvent, useState} from "react";
-import {Todo} from "./Todo.ts";
+import {Todo, TodoStatus} from "./Todo.ts";
 
-type TodoFormProps= {
-addNewTodo: (newTodo: Todo) => void
+type TodoFormProps = {
+    addNewTodo: (newTodo: Todo) => void
 }
 
 export default function TodoForm(props: TodoFormProps) {
 
     const [todoDescription, setTodoDescription] = useState<string>("")
+    const [todoStatus, setTodoStatus] = useState<TodoStatus>("")
 
-    function handleTodoDescription(event:ChangeEvent<HTMLInputElement>){
-    setTodoDescription(event.target.value)
+    function handleTodoDescription(event: ChangeEvent<HTMLInputElement>) {
+        setTodoDescription(event.target.value)
     }
 
-    function handleSubmit(event: FormEvent<HTMLFormElement>){
+    function handleTodoStatus(event: ChangeEvent<HTMLInputElement>) {
+        setTodoStatus(event.target.value)
+    }
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault()
-        const newTodo: Todo = {description: todoDescription}
+        const newTodo: Todo = {description: todoDescription, status: todoStatus}
         props.addNewTodo(newTodo)
         setTodoDescription("")
     }
@@ -23,8 +28,23 @@ export default function TodoForm(props: TodoFormProps) {
     return (
         <form onSubmit={handleSubmit}>
             <label htmlFor="description">Was möchtest du machen?
-                <input type={"text"} name={"todoDescription"} id={"description"} value={todoDescription} onChange={handleTodoDescription}/>
+                <input id={"description"} type={"text"} name={"todoDescription"} value={todoDescription}
+                       onChange={handleTodoDescription}/>
             </label>
+            <label htmlFor={"statusOpen"}>
+                <input id={"statusOpen"} type={"radio"} name={"status"} value={"OPEN"}
+                       checked={todoStatus === "OPEN"}
+                       onChange={handleTodoStatus}
+                />noch unbearbeitet</label>
+            <label htmlFor={"statusInProgress"}>
+                <input id={"statusInProgress"} type={"radio"} name={"status"} value={"IN_PROGRESS"}
+                       checked={todoStatus === "IN_PROGRESS"} onChange={handleTodoStatus}
+                />in Bearbeitung </label>
+            <label htmlFor={"statusDone"}>
+                <input id={"statusDone"} type={"radio"} name={"status"} value={"DONE"}
+                       checked={todoStatus === "DONE"}
+                       onChange={handleTodoStatus}
+                />erledigt </label>
             <button>Speichern</button>
         </form>
     )
